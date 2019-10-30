428bca2f76a75949564a4e252729b6ac1d107150
Jean LETHIEC <jean.lethiec@carbon-it.com>
2019-10-30 17:49:13 +0100

    Added test for OperationsHistory, verifying a call to OperationsService

--- src/app/operations-history/operations-history.component.spec.ts
+++ src/app/operations-history/operations-history.component.spec.ts
@@ -1,21 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OperationsHistoryComponent } from './operations-history.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Injectable, Component, Input } from '@angular/core';
+import { OperationsService } from '../operations.service';
+
+@Component({ selector: 'app-calculator-screen', template: '' })
+class CalculatorScreenComponentStub { @Input() operations: Array<String>; }
+
+class OperationsServiceMock {
+  public getHistory() {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(["4+4 = 8"]), 1000);
+    });
+  }
+}
 
 describe('OperationsHistoryComponent', () => {
   let component: OperationsHistoryComponent;
   let fixture: ComponentFixture<OperationsHistoryComponent>;
+  let operationsService: OperationsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [OperationsHistoryComponent],
-      schemas: [NO_ERRORS_SCHEMA]
+      declarations: [
+        OperationsHistoryComponent,
+        CalculatorScreenComponentStub,
+      ],
+      providers: [
+        { provide: OperationsService, useClass: OperationsServiceMock }
+      ]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+    operationsService = TestBed.get(OperationsService);
+    spyOn(operationsService, 'getHistory').and.callThrough();
+
     fixture = TestBed.createComponent(OperationsHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,5 +44,9 @@ describe('OperationsHistoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call service during initialization', () => {
+    expect(operationsService.getHistory).toHaveBeenCalled();
   });
 });
