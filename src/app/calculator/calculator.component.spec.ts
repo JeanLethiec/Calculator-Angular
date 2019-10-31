e63274e10dab40f1ea6ef6aac55010aec1da2412
Jean LETHIEC <jean.lethiec@carbon-it.com>
2019-10-31 10:25:12 +0100

    Created tests on router-based buttons effectiveness

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,27 +1,60 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { CalculatorComponent } from './calculator.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { Router, Routes } from '@angular/router';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { OperationsHistoryComponent } from '../operations-history/operations-history.component';
+
+@Component({ selector: 'app-calculator-screen', template: '' })
+class OperationsHistoryComponentStub { @Input() operations: Array<String>; }
+
+const routes: Routes = [
+  { path: "history", component: OperationsHistoryComponentStub }
+];
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CalculatorComponent],
+      imports: [
+        RouterTestingModule.withRoutes(routes),
+      ],
+      declarations: [
+        CalculatorComponent,
+        OperationsHistoryComponentStub
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router.initialNavigation();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have button to go to history', fakeAsync(() => {
+    const button = fixture.debugElement.query(By.css('.button'));
+    button.triggerEventHandler('click', null);
+    tick();
+    expect(location.path()).toBe('/history');
+  }))
 });
--- src/app/operations-history/operations-history.component.spec.ts
+++ src/app/operations-history/operations-history.component.spec.ts
@@ -1,17 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { OperationsHistoryComponent } from './operations-history.component';
-import { NO_ERRORS_SCHEMA, Injectable, Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { OperationsService } from '../operations.service';
+import { By } from '@angular/platform-browser';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({ selector: 'app-calculator-screen', template: '' })
 class CalculatorScreenComponentStub { @Input() operations: Array<String>; }
 
 class OperationsServiceMock {
   public getHistory() {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(["4+4 = 8"]), 1000);
-    });
+    return new Promise(() => { });
   }
 }
 
@@ -19,9 +23,14 @@ describe('OperationsHistoryComponent', () => {
   let component: OperationsHistoryComponent;
   let fixture: ComponentFixture<OperationsHistoryComponent>;
   let operationsService: OperationsService;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([]),
+      ],
       declarations: [
         OperationsHistoryComponent,
         CalculatorScreenComponentStub,
@@ -37,9 +46,13 @@ describe('OperationsHistoryComponent', () => {
     operationsService = TestBed.get(OperationsService);
     spyOn(operationsService, 'getHistory').and.callThrough();
 
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
     fixture = TestBed.createComponent(OperationsHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router.initialNavigation();
   });
 
   it('should create', () => {
@@ -49,4 +62,11 @@ describe('OperationsHistoryComponent', () => {
   it('should call service during initialization', () => {
     expect(operationsService.getHistory).toHaveBeenCalled();
   });
+
+  it('should have button to go to calculator', fakeAsync(() => {
+    const button = fixture.debugElement.query(By.css('.button'));
+    button.triggerEventHandler('click', null);
+    tick();
+    expect(location.path()).toBe('/');
+  }));
 });
